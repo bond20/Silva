b9b45eebb611d47c643fe02549096d2fe9779d84
Jun Bond <u2060936@unimail.hud.ac.uk>
2021-03-10 02:32:33 +0000

    Basic Home page

--- app/src/main/java/com/groupproject/silva/MainActivity.java
+++ app/src/main/java/com/groupproject/silva/MainActivity.java
@@ -2,13 +2,60 @@ package com.groupproject.silva;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    
+    TextView sleeptext,infotext;
+    ImageView sleepimg,infoimg;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        sleeptext = (TextView)findViewById(R.id.sleep_text);
+        sleeptext.setOnClickListener(new View.OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                openSleepAnalysis();
+            }
+        });
+        infotext = (TextView)findViewById(R.id.information_text);
+        infotext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openInfo();
+            }
+        });
+        sleepimg = (ImageView)findViewById(R.id.imageView);
+        sleepimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSleepAnalysis();
+            }
+        });
+        sleepimg = (ImageView)findViewById(R.id.imageView2);
+        sleepimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openInfo();
+            }
+        });
+    }
+    
+    private void openInfo() {
+        Intent intent = new Intent(MainActivity.this, Information.class);
+        startActivity(intent);
+    }
+    
+    private void openSleepAnalysis() {
+        Intent intent = new Intent(MainActivity.this, SleepAnalysis.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/groupproject/silva/SleepAnalysis.java
@@ -0,0 +1,14 @@
+package com.groupproject.silva;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SleepAnalysis extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sleep_analysis);
+    }
+}
