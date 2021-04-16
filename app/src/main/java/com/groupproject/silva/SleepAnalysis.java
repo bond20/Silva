b4969d15b2d0573e8ad69d1c675c9b440e444918
Jun Bond <u2060936@unimail.hud.ac.uk>
2021-04-16 15:08:31 +0100

    Simplify sleep analysis

--- app/src/main/java/com/groupproject/silva/SleepAnalysis.java
+++ app/src/main/java/com/groupproject/silva/SleepAnalysis.java
@@ -4,81 +4,116 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.location.SettingInjectorService;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import org.w3c.dom.Text;
 
+import java.util.Calendar;
+
 public class SleepAnalysis extends AppCompatActivity {
     Button button_pop_up;
-    Dialog infoDialog, SelectTimeDialog;
-    ImageView Closepopup, SetTimeImg, ClosepopupRec;
-    TextView SetTimeText;
+    Dialog infoDialog;
+    ImageView Closepopup;
+    TimePickerDialog selection;
+    EditText SetTime,SetAlarm;
+    Button b1;
+    int Chour,Cmin;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep_analysis);
+        Variable();
+        onClick();
+    }
+    
+    private void Variable() {
         infoDialog = new Dialog(SleepAnalysis.this);
         infoDialog.setContentView(R.layout.activity_pop_up_sleep);
         infoDialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         infoDialog.getWindow().getAttributes().windowAnimations = R.style.animation;
         
-        SelectTimeDialog = new Dialog(SleepAnalysis.this);
-        SelectTimeDialog.setContentView(R.layout.activity_pop_up_setrectime);
-        SelectTimeDialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        SelectTimeDialog.getWindow().getAttributes().windowAnimations = R.style.animation;
-        
         button_pop_up = (Button) findViewById(R.id.rec_info);
         Closepopup = infoDialog.findViewById(R.id.close_popup);
-        ClosepopupRec = SelectTimeDialog.findViewById(R.id.close_popup);
-        SetTimeText = (TextView) findViewById(R.id.time_dialog);
-        SetTimeImg = (ImageView) findViewById(R.id.Set_time);
         
-        SetTimeImg.setOnClickListener(new View.OnClickListener() {
+        b1 = (Button)findViewById(R.id.rec_ime_1);
+        
+        SetAlarm = (EditText) findViewById(R.id.setalarm);
+        SetTime = (EditText) findViewById(R.id.time_dialog);
+        SetTime.setInputType(InputType.TYPE_NULL);
+        SetAlarm.setInputType(InputType.TYPE_NULL);
+    }
+    
+    public void onClick(){
+        SetTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SelectTimeDialog.show();
+                SettingT();
             }
         });
-        
-        SetTimeText.setOnClickListener(new View.OnClickListener() {
+        SetAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SelectTimeDialog.show();
+                SettingA();
             }
         });
-        
-        
         button_pop_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 infoDialog.show();
             }
         });
-        
         Closepopup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 infoDialog.dismiss();
             }
         });
-        ClosepopupRec.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SelectTimeDialog.dismiss();
-            }
-        });
     }
     
+    private void SettingA() {
+        Calendar calendar = Calendar.getInstance();
+        Chour = calendar.get(Calendar.HOUR_OF_DAY);
+        Cmin = calendar.get(Calendar.MINUTE);
+        selection = new TimePickerDialog(SleepAnalysis.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                SetTime.setText(String.format("%d:%d", hourOfDay, minute));
+            }
+        },Chour,Cmin,true);
+        selection.show();
+    }
     
+    private void SettingT() {
+        Calendar calendar = Calendar.getInstance();
+        Chour = calendar.get(Calendar.HOUR_OF_DAY);
+        Cmin = calendar.get(Calendar.MINUTE);
+        selection = new TimePickerDialog(SleepAnalysis.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                SetTime.setText(String.format("%d:%d", hourOfDay, minute));
+                if((hourOfDay + 8) >= 24){
+                    hourOfDay = hourOfDay - 24 + 8;
+                    SetAlarm.setText(String.format("%d:%d", hourOfDay, minute));
+                }else if ((hourOfDay + 8) <= 23){
+                    SetAlarm.setText(String.format("%d:%d",hourOfDay+8,minute));
+                }
+            }
+        },Chour,Cmin,true);
+        selection.show();
+    }
 }
