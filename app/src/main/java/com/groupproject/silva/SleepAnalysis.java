1ea01086b7d94c71a24a1477eaad0c875294043f
Jun Bond <u2060936@unimail.hud.ac.uk>
2021-04-21 17:49:10 +0100

    added timer

--- app/src/main/java/com/groupproject/silva/SleepAnalysis.java
+++ app/src/main/java/com/groupproject/silva/SleepAnalysis.java
@@ -11,6 +11,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.location.SettingInjectorService;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.text.InputType;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,16 +23,22 @@ import android.widget.TimePicker;
 
 import org.w3c.dom.Text;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class SleepAnalysis extends AppCompatActivity {
-    Button button_pop_up;
     Dialog infoDialog;
     ImageView Closepopup;
-    TimePickerDialog selection;
-    EditText SetTime,SetAlarm;
-    Button b1;
-    int Chour,Cmin;
+    TextView ShowCurrentTime, Rec_time,SetTimer;
+    ImageView ShowCurrentTimeImg;
+    Button button_pop_up,STimer,RTimer;
+    int Chour, Cmin;
+    
+    long Timeleft = 900000;
+    long TDuration = Timeleft;
+    boolean RunningTime;
+    CountDownTimer countDownTimer;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,26 +56,41 @@ public class SleepAnalysis extends AppCompatActivity {
         
         button_pop_up = (Button) findViewById(R.id.rec_info);
         Closepopup = infoDialog.findViewById(R.id.close_popup);
+        ShowCurrentTimeImg = (ImageView) findViewById(R.id.Set_time);
         
-        b1 = (Button)findViewById(R.id.rec_ime_1);
+        SetTimer = (TextView) findViewById(R.id.setalarm);
+        SetTimer.setInputType(InputType.TYPE_NULL);
+        STimer = (Button) findViewById(R.id.rec_ime_1);
+        RTimer = (Button) findViewById(R.id.rec_ime_3);
         
-        SetAlarm = (EditText) findViewById(R.id.setalarm);
-        SetTime = (EditText) findViewById(R.id.time_dialog);
-        SetTime.setInputType(InputType.TYPE_NULL);
-        SetAlarm.setInputType(InputType.TYPE_NULL);
+        Rec_time = (TextView) findViewById(R.id.rec_time);
+        ShowCurrentTime = (TextView) findViewById(R.id.time_dialog);
+        
     }
     
-    public void onClick(){
-        SetTime.setOnClickListener(new View.OnClickListener() {
+    public void onClick() {
+        STimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StartStop();
+            }
+        });
+        RTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RestTimer();
+            }
+        });
+        ShowCurrentTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SettingT();
             }
         });
-        SetAlarm.setOnClickListener(new View.OnClickListener() {
+        ShowCurrentTimeImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SettingA();
+                SettingT();
             }
         });
         button_pop_up.setOnClickListener(new View.OnClickListener() {
@@ -83,37 +105,86 @@ public class SleepAnalysis extends AppCompatActivity {
                 infoDialog.dismiss();
             }
         });
+        updateText();
     }
+    
+    private void RestTimer() {
+        Timeleft = TDuration;
+        updateText();
+        RTimer.setVisibility(View.INVISIBLE);
+        STimer.setVisibility(View.VISIBLE);
+    }
+    
+    private void StartStop() {
+        if(RunningTime){
+            PTimer();
+        }else {
+            StartTimer();
+        }
+    }
+    
+    private void StartTimer() {
+        countDownTimer = new CountDownTimer(Timeleft,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Timeleft = millisUntilFinished;
+                updateText();
+            }
+            
+            @Override
+            public void onFinish() {
+                RunningTime = false;
+                STimer.setText("Start");
+                STimer.setVisibility(View.INVISIBLE);
+                RTimer.setVisibility(View.VISIBLE);
+            }
+        }.start();
+        STimer.setText("Pause");
+        RTimer.setVisibility(View.INVISIBLE);
+        RunningTime = true;
+    }
+    
+    private void updateText() {
+        int Tmin = (int) (Timeleft/1000) / 60;
+        int Tsec = (int) (Timeleft/1000) % 60;
+        
+        String TextTimer = String.format(Locale.getDefault(), "%02d:%02d", Tmin, Tsec);
+        SetTimer.setText(TextTimer);
+    }
+    
+    private void PTimer() {
+        countDownTimer.cancel();
+        RunningTime = false;
+        STimer.setText("Start");
+        RTimer.setVisibility(View.VISIBLE);
+    }
+    /*
+    
     
     private void SettingA() {
         Calendar calendar = Calendar.getInstance();
         Chour = calendar.get(Calendar.HOUR_OF_DAY);
         Cmin = calendar.get(Calendar.MINUTE);
-        selection = new TimePickerDialog(SleepAnalysis.this, new TimePickerDialog.OnTimeSetListener() {
+        TimePickerDialog selection = new TimePickerDialog(SleepAnalysis.this, new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                SetTime.setText(String.format("%d:%d", hourOfDay, minute));
+                Rec_time.setText(String.format("%d:%d", hourOfDay, minute));
             }
-        },Chour,Cmin,true);
+        }, Chour, Cmin, true);
         selection.show();
-    }
+    }*/
     
     private void SettingT() {
         Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+        ShowCurrentTime.setText(timeFormat.format(calendar.getTime()));
         Chour = calendar.get(Calendar.HOUR_OF_DAY);
         Cmin = calendar.get(Calendar.MINUTE);
-        selection = new TimePickerDialog(SleepAnalysis.this, new TimePickerDialog.OnTimeSetListener() {
-            @Override
-            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                SetTime.setText(String.format("%d:%d", hourOfDay, minute));
-                if((hourOfDay + 8) >= 24){
-                    hourOfDay = hourOfDay - 24 + 8;
-                    SetAlarm.setText(String.format("%d:%d", hourOfDay, minute));
-                }else if ((hourOfDay + 8) <= 23){
-                    SetAlarm.setText(String.format("%d:%d",hourOfDay+8,minute));
-                }
-            }
-        },Chour,Cmin,true);
-        selection.show();
+        if ((Chour + 8) >= 24) {
+            Chour = Chour - 24 + 8;
+            Rec_time.setText(String.format("%d:%d", Chour, Cmin));
+        } else if ((Chour + 8) <= 23) {
+            Rec_time.setText(String.format("%d:%d", Chour + 8, Cmin));
+        }
     }
 }
