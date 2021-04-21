22ecbaff8ec64167090dffe10dedf52bec568a57
amazm <u1958457@unimail.hud.ac.uk>
2021-04-21 18:05:34 +0100

    Added splash animation and changed background colours

--- app/src/main/java/com/groupproject/silva/SplashScreen.java
+++ app/src/main/java/com/groupproject/silva/SplashScreen.java
@@ -6,10 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
-    
-    private static int SPlASH_SCREEN = 3000;
+    private TextView text1;
+    private TextView text2;
+    private ImageView image1;
+    private static int SPlASH_SCREEN = 3500;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +23,17 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        
+        text1 = (TextView) findViewById(R.id.text1);
+        image1 = (ImageView) findViewById(R.id.image1);
+        text2 = (TextView) findViewById(R.id.text2);
+
+        Animation anim = AnimationUtils.loadAnimation(this,R.anim.transition);
+        Animation anim2 = AnimationUtils.loadAnimation(this,R.anim.transition2);
+
+        text1.startAnimation(anim);
+        image1.startAnimation(anim);
+        text2.startAnimation(anim2);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
